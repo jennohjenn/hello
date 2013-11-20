351024087ed3311a8f891dbb7bfcc77e61dfce69
Erika Gidlund <jennelie.1981@gmail.com>
2013-11-20 11:25:46 +0100

    Major change

--- test.php
+++ test.php
@@ -4,6 +4,6 @@
 </head>
 <body>
 <h1>Hello, new version upcoming</h1>
-<?php echo "My name is Tardis, I'm bigger on the inside"; ?>
+<?php echo "My name is Tardis, I'm bigger on the inside, version 3"; ?>
 </body>
 </html>
